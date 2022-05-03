ac67310af7afd6cafc44ae92b5b4dbeb19ebe3a6
Benjamin Carter <benj.james.carter@gmail.com>
2022-05-03 16:29:49 -0700

    stack_queue_shelter completed with whiteboard

--- python/code_challenges/linked_list_zip.py
+++ python/code_challenges/linked_list_zip.py
@@ -1,29 +1,48 @@
 from data_structures.linked_list import LinkedList
 
 
-def zip_lists(a, b=None):
-    result = LinkedList()
+def zip_lists(a, b):
     current_a = a.head
     current_b = b.head
 
-    if current_a is None:
-        return b
-
-    elif current_b is None:
-        return a
-
-    while current_a and current_b:
-        result.append(current_a.value)
+    while current_b and current_a:
+        a.insert_after(current_a.value, current_b.value)
         current_a = current_a.next
-        result.append(current_b.value)
+        if current_a.next:
+            current_a = current_a.next
         current_b = current_b.next
 
-    while current_b:
-        result.append(current_b.value)
-        current_b = current_b.next
+    if current_a is None or current_b is None:
+        if current_b:
+            return b
+        if current_a:
+            return a
 
-    while current_a:
-        result.append(current_a.value)
-        current_a = current_a.next
+    return a
 
-    return
+# def zip_lists(a, b=None):
+#     result = LinkedList()
+#     current_a = a.head
+#     current_b = b.head
+#
+#     if current_a is None:
+#         return b
+#
+#     elif current_b is None:
+#         return a
+#
+#     while current_a and current_b:
+#         result.append(current_a.value)
+#         current_a = current_a.next
+#         result.append(current_b.value)
+#         current_b = current_b.next
+#
+#     while current_b:
+#         result.append(current_b.value)
+#         current_b = current_b.next
+#
+#     while current_a:
+#         result.append(current_a.value)
+#         current_a = current_a.next
+#
+#     return result
--- python/docs/stack_and_queue/README.md
+++ python/docs/stack_and_queue/README.md
@@ -2,11 +2,14 @@
 Using a Linked List as the underlying data storage mechanism, implement both a Stack and a Queue
 
 ## Whiteboard Process
-[Whiteboard]()
+[Whiteboard](docs/stack_queue_animal_shelter/StackQueueShelter.png)
 ## Approach & Efficiency
+We Create two queues in our AnimalShelter __Init__
+Enqueue method checks if input is class Dog, then enqueue to the dog queue, Or if input is class Cat, enqueue to cat queue
+Dequeue, accepts a string 'cat' or 'dog' and returns the animal at the head of that queue, The first in node.
 
-
-
+The Time is 0(1)
+The Space is 0(1)
 
 ## Solution
 You would run stack.py or queue.py in the terminal calling the methods within.
