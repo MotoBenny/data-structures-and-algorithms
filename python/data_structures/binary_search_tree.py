d8b81a10a3ef6d12cc061de7cc1166b03d83f5ff
Benjamin Carter <benj.james.carter@gmail.com>
2022-05-06 13:49:21 -0700

    built Insert method, created find_max per test file

--- python/data_structures/binary_search_tree.py
+++ python/data_structures/binary_search_tree.py
@@ -12,3 +12,22 @@ class BinarySearchTree(BinaryTree):
         self.data = data
         pass
 
+
+    # TODO: Make or import applicable Node class.
+    def insert(self, data):
+        if self.data:
+            if data < self.data:
+                if self.left is None:
+                    self.left = Node(data)
+                else:
+                    self.left.insert(data)
+            elif data > self.data:
+                if self.right is None:
+                    self.right = Node(data)
+                else:
+                    self.right.insert(data)
+        else:
+            self.data = data
+
+    def find_max(self):
+        pass
