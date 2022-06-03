3821a04af68f7997e629a10d10064ac5c2a1ba9d
Benjamin <benj.james.carter@gmail.com>
2022-06-03 15:02:44 -0700

    graph complete

--- python/data_structures/graph.py
+++ python/data_structures/graph.py
@@ -4,13 +4,43 @@ class Graph:
     """
 
     def __init__(self):
-        # initialization here
-        pass
+        self._adjacency_list = {}
 
-    def some_method(self):
-        # method body here
-        pass
+    def add_node(self, value):
+        """
+        add node
+        Arguments: value
+        Returns: The added node
+        Add a node to the graph
+        """
+        vertex = Vertex(value)
+        self._adjacency_list[vertex] = []
+        return vertex
+
+    def size(self):
+        return len(self._adjacency_list)
+
+    def get_neighbors(self, vertex):
+        return self._adjacency_list[vertex]
+
+    def add_edge(self, start_vertex, end_vertex, weight: int = 0):
+        if start_vertex not in self._adjacency_list or end_vertex not in self._adjacency_list:
+            raise KeyError()
+
+        edge = Edge(end_vertex, weight)
+
+        self._adjacency_list[start_vertex].append(edge)
+
+    def get_nodes(self):
+        return self._adjacency_list.keys()
 
 
 class Vertex:
-    pass
+    def __init__(self, value):
+        self.value = value
+
+
+class Edge:
+    def __init__(self, vertex, weight: int):
+        self.vertex = vertex
+        self.weight = weight
